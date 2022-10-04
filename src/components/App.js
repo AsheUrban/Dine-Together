d94583036f223deb924849a7515f9fdc96372b11
Ashe Urban <ashe@goldentongue.com>
2022-10-04 09:12:44 -0700

    Return to main to implement styling. WIP API on AltApproach branch.

--- src/components/App.js
+++ src/components/App.js
@@ -3,13 +3,11 @@ import Header from './Header';
 import SignIn from './SignIn';
 import PostControl from './PostControl';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import RestaurantList from './RestaurantList';
 
 function App(){
   return (
     <Router>
       <Header />
-      <RestaurantList />
       <Routes>
         <Route path='/sign-in' element={<SignIn />} />
         <Route path='/' element={<PostControl />} />
